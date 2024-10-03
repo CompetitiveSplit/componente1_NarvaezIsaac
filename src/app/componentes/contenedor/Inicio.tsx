5edbda812e16e8a2cbec754a972e6dd25d23ac7a
jazzy <narvaez0526@gmail>
2024-10-02 23:19:32 -0500

    primera version

---
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -0,0 +1,36 @@
+export const Inicio = () => {
+    return (
+      <>
+        <div className="inicio-container">
+          <h1>El poema "忽然之间"</h1>
+          <br />
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/h1tNw1aFzSM?si=hmWWJlbgeM4_0ax9" 
+            title="YouTube video player" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+          <br />
+          <div className="columnas">
+            <p>
+              "忽然之间" (Hūrán zhījiān), que se traduce como "De Repente", es una
+              emotiva balada que he decidido considerar como un poema, gracias a los
+              profundos temas humanos que explora, como el dolor de perder a un ser querido. 
+              A lo largo de la canción, se reflexiona sobre la vulnerabilidad y cómo, en 
+              momentos de debilidad, los recuerdos de esa persona especial resurgen, evocando 
+              una mezcla de amor y tristeza que crea una profunda sensación de nostalgia.
+            </p>
+            <p>
+              A pesar de que el tiempo avanza y todo parece efímero, el amor y las memorias 
+              de la persona amada perduran, actuando a veces como un consuelo y, en otras 
+              ocasiones, como un recordatorio de la pérdida. La canción aborda la complejidad 
+              de dejar ir ese amor y el anhelo de que, sin importar lo que ocurra, el vínculo 
+              emocional continúe existiendo, incluso si todo en el mundo desaparece.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
